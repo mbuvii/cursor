ae336888bbf71d725698aeda18b963eb4c90cf3b
mbuvii <143305956+mbuvii@users.noreply.github.com>
2025-01-13 16:55:17 +0300

    Create helpers.js

---
+++ helpers.js
@@ -0,0 +1,12 @@
+function formatDuration(ms) {
+    const seconds = Math.floor(ms / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    return `${days}d ${hours % 24}h ${minutes % 60}m ${seconds % 60}s`;
+}
+
+module.exports = {
+    formatDuration
+};

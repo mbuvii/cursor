e1ee121d3b5f89e0b5ebef7ba3b1ba546e1c5bd2
mbuvii <143305956+mbuvii@users.noreply.github.com>
2025-01-13 17:06:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    GEMINI_API_KEY: 'YOUR_GEMINI_API_KEY',
     BOT_NAME: '𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝗕𝗼𝘁',
-    OWNER_NUMBER: 'YOUR_NUMBER', // with country code
+    OWNER_NUMBER: '254746440595', // Replace with your number (without +)
     START_TIME: Date.now()
 };
